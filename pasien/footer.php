d6c8f3e719975abe405c2f6b5b96eb002a0ebc51
Imanudin07 <146207121+Imanudin07@users.noreply.github.com>
2024-05-10 13:30:10 -0700

    Add files via upload

---
+++ pasien/footer.php
@@ -0,0 +1,28 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.1.0
+    </div>
+    <strong>Copyright &copy; 2024 <a href="https://www.linkedin.com/in/imanudin-imanudin-37a267294/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">by Imanudin </a>.</strong> All rights reserved.
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../dist/js/demo.js"></script>
+</body>
+
+</html>
